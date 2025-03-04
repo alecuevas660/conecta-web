82636cfadd937f7cc7c695b0528b19af93020999
alecuevas660 <alecuevas660@gmail.com>
2025-03-04 16:51:03 -0300

    diosloquiera

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DA6VQzNX.mjs';
-import { manifest } from './manifest_CiO2oDy6.mjs';
+import { manifest } from './manifest_DsPyDSad.mjs';
 
 const serverIslandMap = new Map();;
 
